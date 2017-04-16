cbcd79948d674aa449ea87dfe07984c2aafe88d5
andychow01 <andychow01@163.com>
2017-04-16 21:48:41 +0800

    readdir

--- src/dir_test.c
+++ src/dir_test.c
@@ -1,10 +1,38 @@
 #include <unistd.h>
+#include <dirent.h>
 #include <cstdio>
 
-int main()
+
+void path_test()
 {
     const size_t BUF_SIZE = 128;
     char buf[BUF_SIZE];
+    chdir("/tmp/");
     getcwd(buf, sizeof(buf));
-    printf("current working directory : %s\n", buf);
+    printf("current working directory: %s\n", buf);
+
+    chdir("/home/zhou/source/c_api");
+    getcwd(buf, sizeof(buf));
+    printf("current working directory: %s\n", buf);
 }
+
+
+void traverse_dir()
+{
+    DIR* dp = opendir("./");
+    struct dirent *entry = NULL;
+    while ((entry = readdir(dp)) != NULL)
+    {
+        printf("dir_name: %s\n", entry->d_name);
+    }
+    closedir(dp);
+}
+
+int main()
+{
+    traverse_dir();
+}
+
+
+
+
