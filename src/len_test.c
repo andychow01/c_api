882544ca2801e8414b9a1b6f3b74002d6d07acfa
andychow01 <andychow01@163.com>
2018-04-01 18:59:49 +0800

    add len_test.c

---
+++ src/len_test.c
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <time.h>
+
+int main() {
+    printf("len of int: %d\n", sizeof(int));
+    printf("len of long: %d\n", sizeof(long));
+    printf("len of time_t: %d\n", sizeof(time_t));
+    printf("len of size_t: %d\n", sizeof(size_t));
+}
